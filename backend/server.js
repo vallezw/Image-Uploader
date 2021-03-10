a70f8ded2fd7277cea138e0bb3cb74429a89f30b
Valentin Zwerschke <v.zwerschke@mail.de>
2021-03-10 19:35:31 +0100

    Add basic file uploading structure

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,22 @@ const app = express()
 
 app.use(fileUpload());
 
+// Upload Endpoint
+app.post("/upload", (req, res) => {
+    if(req.files === null){
+        return res.status(400).json({ msg: "No file uploaded" })
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`});
+    })
+})
+
 app.listen(5000, () => console.log("Server Started..."))
