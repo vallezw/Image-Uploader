507690ff9302b941984812a0867956ea0e87f25f
Valentin Zwerschke <v.zwerschke@mail.de>
2021-03-10 23:25:07 +0100

    Add drag and drop box to react card

--- frontend/src/ImageUploadCard.js
+++ frontend/src/ImageUploadCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,9 +6,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import {useDropzone} from 'react-dropzone';
+
+
+
 const useStyles = makeStyles({
   root: {
-    
+    padding: "15px"
   },
   bullet: {
     display: 'inline-block',
@@ -22,7 +26,43 @@ const useStyles = makeStyles({
     marginBottom: 12,
     fontSize: 11,
   },
+  dragndrop: {
+      borderStyle: "dotted",
+      borderColor: "grey",
+      borderWidth: "2px",
+      borderRadius: "5px",
+      padding: "40px",
+  }
 });
+
+// For Dropzone:
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+  };
+  
+  const activeStyle = {
+    borderColor: '#2196f3'
+  };
+  
+  const acceptStyle = {
+    borderColor: '#00e676'
+  };
+  
+  const rejectStyle = {
+    borderColor: '#ff1744'
+  };
 
 export default function ImageUploadCard() {
   const classes = useStyles();
@@ -37,15 +77,36 @@ export default function ImageUploadCard() {
         <Typography className={classes.pos} color="textSecondary">
           File should be Jpeg, Png, ...
         </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <StyledDropzone />
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
+}
+
+function StyledDropzone(props) {
+    const {
+      getRootProps,
+      isDragActive,
+      isDragAccept,
+      isDragReject
+    } = useDropzone({accept: 'image/*'});
+  
+    const style = useMemo(() => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {})
+    }), [
+      isDragActive,
+      isDragReject,
+      isDragAccept
+    ]);
+  
+    return (
+      <div className="container">
+        <div {...getRootProps({style})}>
+          <p>Drag 'n' drop your image here</p>
+        </div>
+      </div>
+    );
 }
