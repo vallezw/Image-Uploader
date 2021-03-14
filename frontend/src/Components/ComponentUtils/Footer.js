b417a65a55eb91d6703162a43ac3edacc0f10ac2
Valentin Zwerschke <v.zwerschke@mail.de>
2021-03-14 13:58:13 +0100

    Add footer text

---
+++ frontend/src/Components/ComponentUtils/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Css/Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p className="copyright">Made by <a href="https://github.com/vallezw">Valentin Zwerschke</a> | <a href="https://github.com/vallezw/Image-Uploader">vallezw/Image-Uploader</a></p>
+        </footer>
+    )
+}
+
+export default Footer
--- frontend/src/Components/Pages/UploadPage.js
+++ frontend/src/Components/Pages/UploadPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../App.css'
+import Footer from '../ComponentUtils/Footer'
 
 import Header from '../ComponentUtils/Header'
 import ImageUploadCard from '../ComponentUtils/ImageUploadCard'
@@ -8,10 +9,13 @@ function UploadPage() {
     return (
         <div>
             <div className="container">
-            <Header />
-            <div className="cardContainer"> 
-                <ImageUploadCard />
-            </div>
+                <Header />
+                <div className="cardContainer"> 
+                    <ImageUploadCard />
+                </div>
+                <div className="footerContainer">
+                    <Footer />
+                </div>
             </div>
         </div>
     )
