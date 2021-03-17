c827f363939183aabf4ad236c45d798ebe134931
Valentin Zwerschke <v.zwerschke@mail.de>
2021-03-17 15:37:26 +0100

    Change Card Position

--- frontend/src/Components/ComponentUtils/SocialMediaShareButtons.js
+++ frontend/src/Components/ComponentUtils/SocialMediaShareButtons.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function SocialMediaShareButtons() {
     return (
         <div>
-            <h1>Social media link</h1>
         </div>
     )
 }
--- frontend/src/Components/ComponentUtils/UploadedImage.js
+++ frontend/src/Components/ComponentUtils/UploadedImage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import './Css/Image.css'
+import './Css/Image.scss'
 
 export default class UploadedImage extends Component {
     state = {
@@ -14,30 +15,29 @@ export default class UploadedImage extends Component {
 
         return (
             <div>
-            <div>
-                <img
-                id="myImg"
-                src={image_url}
-                onClick={() => {
-                    this.setState({ showModal: true, caption: "Uploaded", modalSrc: image_url});
-                }}
-                alt="Uploaded"
-                style={{ width: '100%', maxWidth: '300px' }}
-                />
-            </div>
-    
-            <div
-                id="myModal"
-                className="modal"
-                style={{ display: this.state.showModal ? 'block' : 'none' }}
-            >
-                <div>
-                <span className="close" onClick={() => this.setState({ showModal: false })}>
-                    &times;
-                </span>
-                <img className="modal-content" id="img01" src={this.state.modalSrc} alt="Uploaded"/>
+                <div className="box">
+                    <img
+                    id="myImg"
+                    src={image_url}
+                    onClick={() => {
+                        this.setState({ showModal: true, caption: "Uploaded", modalSrc: image_url});
+                    }}
+                    alt="Uploaded"
+                    />
                 </div>
-            </div>
+    
+                <div
+                    id="myModal"
+                    className="modal"
+                    style={{ display: this.state.showModal ? 'block' : 'none' }}
+                >
+                    <div>
+                        <span className="close" onClick={() => this.setState({ showModal: false })}>
+                            &times;
+                        </span>
+                        <img className="modal-content" id="img01" src={this.state.modalSrc} alt="Uploaded"/>
+                    </div>
+                </div>
             </div>
         );
     }
