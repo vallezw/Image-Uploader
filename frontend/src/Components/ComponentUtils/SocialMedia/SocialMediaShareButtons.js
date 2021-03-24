1b8be060c90502e89633328801c1d3f5930ed767
Valentin Zwerschke <v.zwerschke@mail.de>
2021-03-24 14:45:02 +0100

    Add Social Media redirects

--- frontend/src/Components/ComponentUtils/SocialMedia/SocialMediaShareButtons.js
+++ frontend/src/Components/ComponentUtils/SocialMedia/SocialMediaShareButtons.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './Css/SocialMedia.css'
 
+const URL = window.location.href
+
+const TEXT = `Hey, look at this cool picture I uploaded!`
+
 function SocialMediaShareButtons() {
     return (
         
             <div className="btn_wrap">
                 <span className="socialSpan">Share</span>
                 <div class="shareWrap">
-                    <i class="fab fa-facebook-f"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-instagram"></i>
+                    <a href={`https://www.facebook.com/sharer/sharer.php?u=${URL}`}><i class="fab fa-facebook-f"></i></a>
+                    <a href={`https://twitter.com/intent/tweet?url=${URL}&text=${TEXT}`}><i class="fab fa-twitter"></i></a>
+                    <a href={`whatsapp://send?text=${TEXT}` + ` ${URL}`} data-action="share/whatsapp/share"><i class="fab fa-whatsapp large"></i></a>
                     <i class="fas fa-copy"></i>
                     <i class="fas fa-download"></i>
                 </div>
