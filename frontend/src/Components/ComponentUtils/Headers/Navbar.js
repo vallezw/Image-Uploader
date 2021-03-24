9ac2844b4fad6f8d8f7cf0ff74bd4815f08415e2
Valentin Zwerschke <v.zwerschke@mail.de>
2021-03-24 18:48:21 +0100

    Fix class to className

--- frontend/src/Components/ComponentUtils/Headers/Navbar.js
+++ frontend/src/Components/ComponentUtils/Headers/Navbar.js
@@ -5,13 +5,13 @@ import '../Css/Navbar.css'
 function Navbar() {
     return (
         <header>
-            <a class="logo" href="/"><p className="logo">Image Uploader</p></a>
+            <a className="logo" href="/"><p className="logo">Image Uploader</p></a>
             <nav>
-                <ul class="nav__links">
+                <ul className="nav__links">
                     <li><a href="http://github.com/vallezw/Image-Uploader">About</a></li>
                 </ul>
             </nav>
-            <a class="cta" href="/">Upload</a>
+            <a className="cta" href="/">Upload</a>
         </header>
     )
 }
--- frontend/src/Components/ComponentUtils/SocialMedia/SocialMediaShareButtons.js
+++ frontend/src/Components/ComponentUtils/SocialMedia/SocialMediaShareButtons.js
@@ -3,7 +3,7 @@ import './Css/SocialMedia.css'
 
 const URL = window.location.href
 
-const SERVER_URL = "http://localhost:5000/download/" + URL.split(':3000/upload/')[1]
+const SERVER_URL = "http://localhost:5000/download/" + URL.split('/upload/')[1]
 // TODO: Make it not hardcoded
 const TEXT = `Hey, look at this cool image I uploaded!`
 
@@ -12,12 +12,12 @@ function SocialMediaShareButtons() {
         
             <div className="btn_wrap">
                 <span className="socialSpan">Share</span>
-                <div class="shareWrap">
-                    <a href={`https://www.facebook.com/sharer/sharer.php?u=${URL}`} className="iconButton"><i class="fab fa-facebook-f"></i></a>
-                    <a href={`https://twitter.com/intent/tweet?url=${URL}&text=${TEXT}`} className="iconButton"><i class="fab fa-twitter"></i></a>
-                    <a href={`whatsapp://send?text=${TEXT}%0a${URL}`} data-action="share/whatsapp/share" className="iconButton"><i class="fab fa-whatsapp"></i></a>
-                    <button onClick={() => {navigator.clipboard.writeText(URL)}} className="iconButton" ><i class="fas fa-copy"></i></button>
-                    <a download="UploadedImage" href={SERVER_URL} className="iconButton"><i class="fas fa-download"></i></a>
+                <div className="shareWrap">
+                    <a href={`https://www.facebook.com/sharer/sharer.php?u=${URL}`} className="iconButton"><i className="fab fa-facebook-f"></i></a>
+                    <a href={`https://twitter.com/intent/tweet?url=${URL}&text=${TEXT}`} className="iconButton"><i className="fab fa-twitter"></i></a>
+                    <a href={`whatsapp://send?text=${TEXT}%0a${URL}`} data-action="share/whatsapp/share" className="iconButton"><i className="fab fa-whatsapp"></i></a>
+                    <button onClick={() => {navigator.clipboard.writeText(URL)}} className="iconButton" ><i className="fas fa-copy"></i></button>
+                    <a download="UploadedImage" href={SERVER_URL} className="iconButton"><i className="fas fa-download"></i></a>
                 </div>
             </div>
         
