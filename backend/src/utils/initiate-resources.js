a8cde41f2e5c42ddf712521dc99492a789755e34
Valentin Zwerschke <v.zwerschke@mail.de>
2021-04-01 22:56:21 +0200

    Add folder exists check

--- backend/src/utils/initiate-resources.js
+++ backend/src/utils/initiate-resources.js
@@ -1,11 +1,20 @@
 const { applyMiddlewares } = require('../middlewares');
 const { renderRoutes } = require('../routes/index');
 const removeImages = require('./remove-images');
+const fs = require('fs');
+const { endpoints } = require('../constants');
+
 
 const initiateResources = (app) => {
     applyMiddlewares(app);
 
     renderRoutes(app);
+
+    const dir = `${__dirname}/..` + endpoints.UPLOAD_STATIC_DIRECTORY
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
 
     if(process.env.REMOVE_IMAGES === 'true') {
         removeImages();
