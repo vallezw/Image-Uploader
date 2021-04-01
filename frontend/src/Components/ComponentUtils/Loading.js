a86ba4afbdfd5fab1ab30b51f581390d6185662a
Valentin Zwerschke <v.zwerschke@mail.de>
2021-04-01 15:08:12 +0200

    Add new loading animation

--- frontend/src/Components/ComponentUtils/Loading.js
+++ frontend/src/Components/ComponentUtils/Loading.js
@@ -1,15 +1,11 @@
-import { HashLoader } from 'react-spinners';
-
 import '../../App.css'
 
 
 export default function Loading() {
     return (
       <div className="loading">
-        <HashLoader
-          color={'#fff'}
-          size={100}
-        />
+        <lottie-player src="https://assets7.lottiefiles.com/datafiles/9jPPC5ogUyD6oQq/data.json"  background="transparent"  speed="1"  style={{width: "500px", height: "500px"}} loop  autoplay></lottie-player>
+
       </div>
     )
 }
