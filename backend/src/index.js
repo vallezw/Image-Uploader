03df307219207a7a4937ca8bb3bf95df21d527ef
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-04-01 16:37:40 +0200

    Create backend/src/utils/load-env.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
+require('./utils/load-env')
+
 const Server = require('./server');
 
 const server = new Server(process.env.PORT || 5000);
+
 server.start();
--- backend/src/utils/initiate-resources.js
+++ backend/src/utils/initiate-resources.js
@@ -1,9 +1,16 @@
 const { applyMiddlewares } = require('../middlewares');
 const { renderRoutes } = require('../routes/index');
+const removeImages = require('./remove-images');
 
 const initiateResources = (app) => {
     applyMiddlewares(app);
+
     renderRoutes(app);
+
+    if(process.env.REMOVE_IMAGES === 'true') {
+        console.log('yy');
+        removeImages();
+    };
 };
 
 module.exports = initiateResources;
