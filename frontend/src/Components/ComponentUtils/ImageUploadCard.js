ec96e6d531aca4fdc416e778cfa0b7c696cc5549
Valentin Zwerschke <v.zwerschke@mail.de>
2021-04-02 12:55:00 +0200

    Center card

--- frontend/src/Components/ComponentUtils/ImageUploadCard.js
+++ frontend/src/Components/ComponentUtils/ImageUploadCard.js
@@ -18,7 +18,9 @@ const useStyles = makeStyles({
     paddingTop: "10px",
     paddingBottom: "10px",
     borderRadius: "7px",
-    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    display: "grid",
+    placeItems: "center"
   },
   headerText: {
       fontFamily: "roboto",
