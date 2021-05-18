3cb4be209c4fced195893888f2a61b0384e9d216
Valentin Zwerschke <v.zwerschke@mail.de>
2021-05-18 19:45:38 +0200

    Add blank target

--- frontend/src/Components/ComponentUtils/Footer.js
+++ frontend/src/Components/ComponentUtils/Footer.js
@@ -5,7 +5,7 @@ import './Css/Footer.css'
 function Footer() {
     return (
         <footer>
-            <p className="copyright">Made by <a href="https://github.com/vallezw">Valentin Zwerschke</a> | <a href="https://github.com/vallezw/Image-Uploader">vallezw/Image-Uploader</a></p>
+            <p className="copyright">Made by <a href="https://github.com/vallezw" target="_blank">Valentin Zwerschke</a> | <a href="https://github.com/vallezw/Image-Uploader" target="_blank" >vallezw/Image-Uploader</a></p>
         </footer>
     )
 }
